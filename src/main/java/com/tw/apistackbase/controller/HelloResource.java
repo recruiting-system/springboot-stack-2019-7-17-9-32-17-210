041469d7e6d2e30967659e66cc56ac6088aa1d0a
linwenjun <linwenjun@163.com>
2019-07-17 09:32:45 +0000

    first commit

---
+++ src/main/java/com/tw/apistackbase/controller/HelloResource.java
@@ -0,0 +1,27 @@
+package com.tw.apistackbase.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by jxzhong on 18/08/2017.
+ */
+@RestController
+@RequestMapping("/hello")
+public class HelloResource {
+
+    private final Logger log = Logger.getLogger(this.getClass().getName());
+
+    @GetMapping(path = "/{userName}", produces = {"application/json"})
+    public ResponseEntity<String> getAll(@PathVariable String userName) {
+
+        return ResponseEntity.ok("Hello:" + userName);
+    }
+
+
+}
